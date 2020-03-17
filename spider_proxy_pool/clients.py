762aa694eacbb78c21b2a76cc4417d514b6c04ae
joshua <nyxiang888@gmail.com>
2020-03-17 10:00:17 +0800

    更改链接方式

--- spider_proxy_pool/clients.py
+++ spider_proxy_pool/clients.py
@@ -4,72 +4,116 @@
 # @Email   : wytheli168@163.com
 # @Time    : 19-12-20 下午2:23
 # @Description:
+import pymysql
 import redis
 from pymongo import MongoClient
 from scrapy.utils.project import get_project_settings
+from spider_proxy_pool import constants
 
-# 获取settings中的配置信息
-# settings = get_project_settings()
-# for key, value in settings.items():
-#     print(key, value)
-settings = get_project_settings()
 
-mongo_conn = MongoClient(
-    host=settings["MONGO_HOST"],
-    port=settings["MONGO_PORT"],
-    username=settings["MONGO_USERNAME"],
-    password=settings["MONGO_PASSWORD"]
-)
-db = mongo_conn[settings["MONGO_DB"]]
+class Mongo(object):
+    mongo_conn = None
 
-mongo_conn_1 = MongoClient(
-    host=settings["MONGO_HOST_1"],
-    port=settings["MONGO_PORT_1"],
-    username=settings["MONGO_USERNAME_1"],
-    password=settings["MONGO_PASSWORD_1"]
-)
-db_1 = mongo_conn_1[settings["MONGO_DB_1"]]
+    def __init__(self,
+                 mongo_host="localhost",
+                 mongo_port=27017,
+                 mongo_user="root",
+                 mongo_password="",
+                 mongo_db=""):
+        self.mongo_host = mongo_host
+        self.mongo_port = mongo_port
+        self.mongo_user = mongo_user
+        self.mongo_password = mongo_password
+        self.mongo_db = mongo_db
+
+        if not self.mongo_conn:
+            self.mongo_conn = MongoClient(
+                host=self.mongo_host,
+                port=self.mongo_port,
+                username=self.mongo_user,
+                password=self.mongo_password
+            )
+
+    def get_mongo_conn(self):
+        return self.mongo_conn
+
+    def get_db(self):
+        return self.mongo_conn[self.mongo_db]
+
+    def __del__(self):
+        self.mongo_conn.close()
+
+
+mongo = Mongo(mongo_host=constants.MONGO_HOST,
+              mongo_port=constants.MONGO_PORT,
+              mongo_user=constants.MONGO_USERNAME,
+              mongo_password=constants.MONGO_PASSWORD,
+              mongo_db=constants.MONGO_DB)
+mongo_conn = mongo.get_mongo_conn()
+db = mongo.get_db()
+# mongo_conn = MongoClient(
+#     host=constants.MONGO_HOST,
+#     port=constants.MONGO_PORT,
+#     username=constants.MONGO_USERNAME,
+#     password=constants.MONGO_PASSWORD
+# )
+# # client = MongoClient(host=config.HOST, port=config.PORT, username=config.USERNAME, password=config.PASSWORD, retryWrites=False)
+# db = mongo_conn[constants.MONGO_DB]
+
+# mysql_conn = pymysql.connect(
+#     host=constants.MYSQL_HOST,
+#     user=constants.MYSQL_USERNAME,
+#     password=constants.MYSQL_PASSWORD,
+#     db=constants.MYSQL_DB,
+#     # cursorclass=pymysql.cursors.DictCursor,  # 修改返回的数据类型为字典
+#     charset='utf8mb4'
+# )
 
 # 创建redis连接池
 redis_conn_pool = redis.ConnectionPool(
-    host=settings["REDIS_HOST"],
-    port=settings["REDIS_PORT"],
-    db=settings["REDIS_DB"],
-    password=settings["REDIS_PASSWORD"]
+    host=constants.REDIS_HOST,
+    port=constants.REDIS_PORT,
+    db=constants.REDIS_DB,
+    password=constants.REDIS_PASSWORD
 )
 # 创建连接对象
 redis_conn = redis.Redis(connection_pool=redis_conn_pool)
 
-# 线下
-redis_conn_pool_1 = redis.ConnectionPool(
-    host=settings["REDIS_HOST_LOCALHOST"],
-    port=settings["REDIS_PORT"],
-    db=settings["REDIS_DB"],
-    password=settings["REDIS_PASSWORD"]
+
+# 创建本地redis连接池
+redis_conn_pool_l = redis.ConnectionPool(
+    host=constants.REDIS_HOST_L,
+    port=constants.REDIS_PORT_L,
+    db=constants.REDIS_DB_L,
+    password=constants.REDIS_PASSWORD_L
 )
-redis_conn_1 = redis.Redis(connection_pool=redis_conn_pool_1)
+# 创建连接对象
+redis_conn_l = redis.Redis(connection_pool=redis_conn_pool_l)
 
+if __name__ == "__main__":
+    # 字符串操作
+    # redis_conn.set("start_time", "2019-10-10")
+    # res1 = redis_conn.get("start_time").decode()
+    # print(res1)
+    # redis_conn.setex("骑士"+"time_node_aaa", 300, "aaaa")
+    # print(redis_conn.get("骑士" + "time_node_aaa"))
 
-if __name__ == '__main__':
-    # 本地mongo备份到线上
-    # res = db['proxies'].find()
+    # res = redis_conn.smembers("proxies")
     # print(res)
-    # for sing in res:
-    #     data = {
-    #         'host': sing['host'],
-    #         'port': sing['port'],
-    #         'location': sing['location'],
-    #         'operators': sing['operators'],
-    #         'is_support_https': sing['is_support_https'],
-    #         'is_support_post': sing['is_support_post'],
-    #         'type': sing['type'],
-    #         'delay': sing['delay'],
-    #         'relative_time': sing['relative_time'],
-    #         'create_time': sing['create_time'],
-    #     }
-    #     res_id = db_1['proxies'].insert_one(data)
-    #     print(res_id)
-    proxies = redis_conn_1.smembers("proxies")
-    for p in proxies:
-        print(p)
-        redis_conn.sadd("proxies", p)
+
+    # 列表操作
+    # redis_conn.lpush("login_user_list", "aaa")
+    # redis_conn.lpush("login_user_list", "bbb")
+    # list_len = redis_conn.llen("login_user_list")
+    # print(list_len)
+    # res_list = redis_conn.lrange("login_user_list", 0, list_len-1)
+    # print(res_list)
+    # res = db["mn_sports_qq_nba_text"].delete_many({"$and": [{"create_time": {"$gte": '2020-01-08'}}, {"create_time": {"$lte": '2020-01-09'}}]})
+    # print(res.deleted_count)
+
+    # res = redis_conn.srandmember("proxies")
+    # print(res)
+
+    redis_conn.set("aaa", 1)
+    res = redis_conn.get("aaa")
+    print(res)
---
+++ spider_proxy_pool/constants.py
@@ -0,0 +1,33 @@
+#!/usr/bin/env python
+# encoding: utf-8
+'''
+@author: yuxiang
+@license: (C) Copyright 2013-2017, Node Supply Chain Manager Corporation Limited.
+@contact: nyxiang888@gmail.com
+@software: garner
+@file: constants.py
+@time: 20-3-17 上午9:56
+@desc:
+'''
+# mongo
+MONGO_USERNAME = "root"
+MONGO_PASSWORD = "shangHaimeanergy168"
+# MONGO_HOST = "127.0.0.1"
+MONGO_HOST = "139.129.229.223"
+MONGO_PORT = 27017
+MONGO_DB = "db_test"
+
+
+# redis
+# REDIS_HOST = "127.0.0.1"
+REDIS_HOST = "139.129.229.223"
+REDIS_PORT = 6379
+REDIS_PASSWORD = "shangHaimeanergy168"
+REDIS_DB = 1
+
+
+# REDIS_HOST_L = "127.0.0.1"
+REDIS_HOST_L = "139.129.229.223"
+REDIS_PORT_L = 6379
+REDIS_PASSWORD_L = "shangHaimeanergy168"
+REDIS_DB_L = 1
